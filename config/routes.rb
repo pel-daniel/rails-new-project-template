2559530d31a7cabc71cf3e57776e6e1ecc66de3d
DanielGarcia <pel_daniel@hotmail.com>
2014-11-25 05:43:28 -0600

    Root path to login screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Resolutionary::Application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+    end
+
+    unauthenticated :user do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
